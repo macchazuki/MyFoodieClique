ab0f91523500fbf58ffed5d647a2d380886964ca
marcus-pzj <marcuszhengjie@gmail.com>
2018-06-18 03:08:17 -0700

    Basic title

---
+++ my-foodie-clique/src/Components/App.js
@@ -0,0 +1,5 @@
+import Main from './Main'
+
+const App = Main
+
+export default App
---
+++ my-foodie-clique/src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+class Main extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <h1>MyFoodieClique</h1>
+        )
+    }
+}
+export default Main
